ce4aa14d805c0cc9c14fca3eb200444e9b7c2950
Joseph Walker <99849204+joewxlker@users.noreply.github.com>
2023-11-29 15:40:25 +0800

    fix spacing on landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function HomePage() {
   return (
     <main className={`flex min-h-screen flex-col items-center justify-center gap-5 text-black ${layout}`}>
       <Header config={projectData} layout={layout} />
-      <div id="landing" className="xl:pt-40 lg:pt-40 pt-20 flex flex-col gap-40 xl:px-40 lg:px-20 md:px-20 px-2 max-w-[2000px] w-screen">
+      <div id="landing" className="xl:pt-40 lg:pt-40 pt-28 flex flex-col xl:gap-40 lg:gap-40 gap-28 xl:px-40 lg:px-20 md:px-20 px-2 max-w-[2000px] w-screen">
         <Landing config={projectData}/>
       </div>
       <div id="about-wrapper" className={wrapper}>
